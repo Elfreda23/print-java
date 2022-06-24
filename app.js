66c1fab15dd6fe67cb461a626465516706150154
Elfreda Overare <82799773+Elfreda23@users.noreply.github.com>
2022-06-24 16:31:56 +0100

    using .write method

---
+++ app.js
@@ -0,0 +1,15 @@
+let Efe = {
+    name: "Overare Elfreda",
+    height: "8.6ft",
+    country: "Nigeria",
+}
+
+document.write(Efe.name,);
+document.write("<br/>");
+document.write(Efe.height);
+document.write("<br/>");
+document.write(Efe.country);
+
+
+
+
